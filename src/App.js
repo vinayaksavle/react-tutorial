f83af4bf30c0d45dc43085de3afdfb445fd9cfcb
Vinayak Savale <vinayak.savle@gmail.com>
2020-09-19 22:17:15 +0530

    First React Tutoral App

--- src/App.js
+++ src/App.js
@@ -1,26 +1,54 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import Table from './Table';
+import Form from './Form';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    characters: [
+      // {
+      //   name: 'Charlie',
+      //   job: 'Janitor',
+      // },
+      // {
+      //   name: 'Mac',
+      //   job: 'Bouncer',
+      // },
+      // {
+      //   name: 'Dee',
+      //   job: 'Aspring actress',
+      // },
+      // {
+      //   name: 'Dennis',
+      //   job: 'Bartender',
+      // }, 
+    ],
+  }
+
+  removeCharacter = (index) => {
+    const { characters } = this.state;
+
+    this.setState({
+      characters: characters.filter((characters, i) => {
+        return i !== index;
+      }),
+    })
+  }
+
+  handleSubmit = (character) => {
+    this.setState({ characters: [...this.state.characters, character] })
+  }
+
+  render() {
+    const { characters } = this.state;
+
+    return (
+      <div className="container">
+        <Table characterData={characters} removeCharacter={this.removeCharacter} />
+        <Form handleSubmit={this.handleSubmit} />
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
